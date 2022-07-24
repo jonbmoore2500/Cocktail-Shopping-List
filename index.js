a22db2926dc59b71707fdb75191f7ff3fff3d2b6
Jonathan <jonathanmoorevla@gmail.com>
2022-07-24 17:21:20 -0500

    WIP - Create initial html layout, begin js process

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // index.js for Cocktail Shopping List App
+
+
+const searchForm = document.getElementById('searchForCocktail')
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault(); 
+    console.log(e.target)
+})
