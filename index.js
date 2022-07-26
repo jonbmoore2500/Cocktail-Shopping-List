4df9e61662b458bcf4fdb48d89581a4cd7badbfc
Jonathan <jonathanmoorevla@gmail.com>
2022-07-26 09:16:06 -0500

    Update search functionality, develop page layout

--- index.js
+++ index.js
@@ -2,8 +2,41 @@
 
 
 const searchForm = document.getElementById('searchForCocktail')
-
 searchForm.addEventListener('submit', (e) => {
-    e.preventDefault(); 
-    console.log(e.target)
+    e.preventDefault();   
+    handleSearchGet(e);
 })
+function handleSearchGet(e) {
+    let searchInput = e.target.querySelector('#cocktailSearch').value
+    fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}&key=1`, {
+        method: 'GET', 
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+    })
+    //searchInput.reset();
+    // 'http://universities.hipolabs.com/search?country=United+States'
+    // `http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput}`
+}
+
+const randForm = document.getElementById('randomCocktail')
+randForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleRandGet(e)
+})
+function handleRandGet(e) {
+    fetch(`http://www.thecocktaildb.com/api/json/v1/1/random.php`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+    })
+}
